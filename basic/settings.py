67a8e28803a0e96ccdf14ae3ed5e9a47e4200c82
Sandaly Singh <56186048+sandy502@users.noreply.github.com>
2022-07-30 00:14:09 +0530

    updated

--- basic/settings.py
+++ basic/settings.py
@@ -39,8 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'members.apps.MembersConfig',
-    'blog.apps.BlogConfig',
     'tasks',
 ]
 
